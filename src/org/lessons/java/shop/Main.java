c2d8840edbfb4b7b2ec8dff36d68462f7d0cd995
FrancescoCellitti <francescocellitti@virgilio.it>
2025-10-10 16:21:43 +0200

    added private at the attributes and added getter and setter method

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -2,19 +2,20 @@ package org.lessons.java.shop;
 
 public class Main {
     public static void main(String[] args) {
-        Prodotto pc = new Prodotto(100, 22, "computer");
+         Prodotto pc = new Prodotto(100, 22, "computer");        /* Prodotto pc = new Prodotto(); */
 
         System.out.println("=== INFORMAZIONI PRODOTTO ===");
-        
+        pc.setName("ciao");
         // Chiama le funzioni direttamente (non dentro println!)
         pc.completeName();    // Stampa: codice-nome
-        pc.basePrice();       // Stampa: prezzo base
+        /* pc.basePrice();   */     // Stampa: prezzo base
         pc.ivaPrice();        // Stampa: prezzo con IVA
         
         System.out.println("\n=== ACCESSO DIRETTO AI CAMPI ===");
-        System.out.println("Codice: " + pc.code);
-        System.out.println("Nome: " + pc.name);
-        System.out.println("Prezzo: " + pc.price);
-        System.out.println("IVA: " + pc.iva + "%");
+        System.out.println("Codice: " + pc.getCode());
+        System.out.println("Nome: " + pc.getName());
+        System.out.println("Prezzo: " + pc.getPrice());
+        System.out.println("IVA: " + pc.getIva() + "%");
+        
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,29 +1,71 @@
 package org.lessons.java.shop;
 public class Prodotto {
-    public int code;
-    public String name;
-    public String description;
-    public float price;
-    public int iva;
-    public float ivaPrice;
+    private int code;
+    private String name;
+    private String description;
+    private float price;
+    private int iva;
+    private float ivaPrice;
+
+    public Prodotto(){
+        this.code = (int)(Math.random() * 100000) + 1; 
+    }
 
     public Prodotto(int price, int iva, String name){
-        this.code = (int)(Math.random() * 100000) + 1; 
+        this.code = (int)(Math.random() * 100000) + 1;
         this.price = price;
         this.iva = iva;
         this.name = name;
     }
 
-    public void basePrice(){
+    /* public void basePrice(){
+
         System.out.println(this.price);
-    }
+    } */
 
     public void ivaPrice(){
        this.ivaPrice = this.price + ((this.price * this.iva)/100);
        System.out.println(ivaPrice);
     }
+     
 
     public void completeName(){
         System.out.println(code+"-"+name);
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setdescription(String description){
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setPrice(int price){
+        this.price = price;
+    }
+
+    public float getPrice(){
+        return price;
+    }
+    
+    public void setIva(int iva){
+        this.iva = iva;
+    }
+
+
+    public int getIva(){
+        return iva;
+    }
+
+    public int getCode(){
+        return code;
     }
 }
